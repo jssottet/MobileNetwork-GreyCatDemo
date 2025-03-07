2f11ce91619a2caf556c3520e7bdaec8ca2d9379
Grégory NAIN <gregory.nain@datathings.com>
2025-03-07 21:16:37 +0100

    - Reorganized files in a "GreyCat standard" way - Modified data model to leverage temporal graph capabilities - Updated loaders to populate the graph

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@
 #Install Greycat
 #./install.sh
 #Start the server (local setup for my machine)
-/home/sottet/TechProjects/GreyCat/newVersion/6.10.109-stable/bin/greycat run 
+#/home/sottet/TechProjects/GreyCat/newVersion/6.10.109-stable/bin/greycat run 
+rm -rf ./gcdata && /home/sottet/TechProjects/GreyCat/newVersion/6.10.109-stable/bin/greycat serve --user=1
 
 #Server configuration
 #serve --user=1
